a184832fb0d1d34011d9886cc0b6093b2db1218d
蒋瑶 <landn172@gmail.com>
2017-12-06 10:28:09 +0800

    feat: 添加首屏 offine

--- src/sw.js
+++ src/sw.js
@@ -2,4 +2,7 @@ importScripts('workbox-sw.prod.js')
 
 const workboxSW = new WorkboxSW({ clientsClaim: true });
 
+const networkFirst = workboxSW.strategies.networkFirst();
+workboxSW.router.registerRoute('/', networkFirst);
+
 workboxSW.precache([]);
